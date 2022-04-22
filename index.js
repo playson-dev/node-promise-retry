88363fe53b19e3e9f7a98976dae23985045a19b9
Sergey Franchuk <s.franchuk@pls.life>
2022-04-22 16:16:59 +0300

    reformat code

--- index.js
+++ index.js
@@ -6,47 +6,47 @@ var retry = require('retry');
 var hasOwn = Object.prototype.hasOwnProperty;
 
 function isRetryError(err) {
-    return err && err.code === 'EPROMISERETRY' && hasOwn.call(err, 'retried');
+  return err && err.code === 'EPROMISERETRY' && hasOwn.call(err, 'retried');
 }
 
 function promiseRetry(fn, options) {
-    var temp;
-    var operation;
+  var temp;
+  var operation;
 
-    if (typeof fn === 'object' && typeof options === 'function') {
-        // Swap options and fn when using alternate signature (options, fn)
-        temp = options;
-        options = fn;
-        fn = temp;
-    }
+  if (typeof fn === 'object' && typeof options === 'function') {
+    // Swap options and fn when using alternate signature (options, fn)
+    temp = options;
+    options = fn;
+    fn = temp;
+  }
 
-    operation = retry.operation(options);
+  operation = retry.operation(options);
 
-    return new Promise(function (resolve, reject) {
-        operation.attempt(function (number) {
-            Promise.resolve()
-            .then(function () {
-                return fn(function (err) {
-                    if (isRetryError(err)) {
-                        err = err.retried;
-                    }
+  return new Promise(function(resolve, reject) {
+    return operation.attempt(function(number) {
+      return Promise.resolve()
+        .then(function() {
+          return fn(function(err) {
+            if (isRetryError(err)) {
+              err = err.retried;
+            }
 
-                    throw errcode(new Error('Retrying'), 'EPROMISERETRY', { retried: err });
-                }, number);
-            })
-            .then(resolve, function (err) {
-                if (isRetryError(err)) {
-                    err = err.retried;
+            throw errcode(new Error('Retrying'), 'EPROMISERETRY', { retried: err });
+          }, number);
+        })
+        .then(resolve, function(err) {
+          if (isRetryError(err)) {
+            err = err.retried;
 
-                    if (operation.retry(err || new Error())) {
-                        return;
-                    }
-                }
+            if (operation.retry(err || new Error())) {
+              return;
+            }
+          }
 
-                reject(err);
-            });
+          reject(err);
         });
     });
+  });
 }
 
 module.exports = promiseRetry;
